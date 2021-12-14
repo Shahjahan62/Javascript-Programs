4d0ce7525a0ecb48c2cb81289c935cb46404e0be
Shah-jahan Mukhtar <Shahjahanmukhtar62@gmail.com>
2021-12-14 14:21:06 +0500

    TestCase

---
+++ testProblem.js
@@ -0,0 +1,43 @@
+
+
+
+function Airport(startPoint,endPoint,cost) {
+
+
+    return startPoint;
+}
+
+let a= Airport[
+    {
+        start: 'ISB',
+        end: 'LHR',
+        cost: 1000
+    },
+    {
+        start: 'LHR',
+        end: 'NYC',
+        cost: 750
+    },
+    {
+        start: 'CBS',
+        end: 'NYC',
+        cost: 775
+    },
+    {
+        start: 'ISB',
+        end: 'CBS',
+        cost: 575
+    },
+    {
+        start: 'CBS',
+        end: 'GRC',
+        cost: 731
+    },
+    {
+        start: 'NYC',
+        end: 'GRC',
+        cost: 459
+    }
+]
+
+console.log(a);
